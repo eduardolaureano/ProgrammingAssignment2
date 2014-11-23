7b4660a06331c3087872c0f8b7ad193a8efd971a
eduardolaureano <eduardo_laureano@hotmail.com>
2014-11-23 12:19:48 -0800

    R programming class assignment commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This .R will contain the full solution with both needed functions
 
-## Write a short comment describing this function
-
+## The initial function "makeCacheMatrix" will create a matrix that inverts 
+## the original matrix. 
+## It uses the same logic of the make vector example
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) i <<- solve
+    getsolve <- function() i
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## The second  function "cacheSolve" will check the cache for a pre-computed result 
+## If a cached result doesn't exist, it will calculate it and update the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getsolve()
+    if(!is.null(i)) {
+        message("returning cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setsolve(i)
+    i
 }
